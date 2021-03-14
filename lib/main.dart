468db9e6ffb924a439286b8a32429aebe7f0c817
pythonhubpy <pythonhub.py@gmail.com>
2021-03-14 10:28:48 +0530

    Firebase Auth Demo Complete File

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import "package:flutter/material.dart";
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,264 +12,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: FirebaseAuthDemo(),
       debugShowCheckedModeBanner: false,
-      home: TabbedSignIn(),
     );
   }
 }
 
-class TabbedSignIn extends StatefulWidget {
+class FirebaseAuthDemo extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  @override
-  _TabbedSignInState createState() => _TabbedSignInState();
-}
-
-class _TabbedSignInState extends State<TabbedSignIn> {
-  bool switched = false;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child: SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.redAccent,
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.redAccent,
-          child: Container(
-            margin: EdgeInsets.only(bottom: 50),
-            child: TabBar(
-              labelColor: Colors.redAccent,
-              labelStyle: TextStyle(fontSize: 30, color: Colors.redAccent, fontWeight: FontWeight.bold),
-              unselectedLabelColor: Colors.white,
-              unselectedLabelStyle: TextStyle(fontSize: 20, color: Colors.redAccent, fontWeight: FontWeight.bold),
-              indicatorSize: TabBarIndicatorSize.label,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(60),
-                color: Colors.white
-              ),
-              tabs: [
-                Tab(
-                 child: Container(
-                   decoration: BoxDecoration(
-                     border: Border.all(color: Colors.white, width: 1),
-                     borderRadius: BorderRadius.circular(60),
-                   ),
-                   child: Align(alignment: Alignment.center, child: Text("Sign In",)),
-                 ),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                style: TextStyle(fontSize: 22, color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Email Address",
+                  hintStyle: TextStyle(fontSize: 22, color: Colors.black),
                 ),
-                Tab(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 1),
-                      borderRadius: BorderRadius.circular(60),
-                    ),
-                    child: Align(alignment: Alignment.center, child: Text("Sign Up",)),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                style: TextStyle(fontSize: 22, color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: TextStyle(fontSize: 22, color: Colors.black),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await _firebaseAuth.createUserWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: _passwordController.text);
+                          },
+                          child: Text('Register'))),
+                  SizedBox(
+                    width: 10,
                   ),
-                )
-              ],
-            ),
+                  Expanded(
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await _firebaseAuth
+                                .signInWithEmailAndPassword(
+                                    email: _emailController.text,
+                                    password: _passwordController.text)
+                                .then((value) => print('Login Successful'));
+                          },
+                          child: Text('Login')))
+                ],
+              )
+            ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                padding: EdgeInsets.only(left: 30, right: 30),
-                child: Form(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Login", style: TextStyle(fontSize: 80, color: Colors.white, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 50,),
-                      TextFormField(
-                        showCursor: false,
-                        style: TextStyle(fontSize: 25, color: Colors.white),
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.email, color: Colors.white, size: 25,),
-                          hintText: "Email Address",
-                          hintStyle: TextStyle(fontSize: 25, color: Colors.white),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.5),
-                            borderRadius: BorderRadius.circular(50)
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2),
-                              borderRadius: BorderRadius.circular(50)
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2),
-                              borderRadius: BorderRadius.circular(50)
-                          )
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text('Remember me', style: TextStyle(fontSize: 20, color: Colors.white),),
-                          Switch(
-                            value: switched,
-                            onChanged: (value){
-                              setState(() {
-                                switched = value;
-                              });
-                            },
-                            activeColor: Colors.white,
-                            inactiveTrackColor: Colors.grey[300],
-                            inactiveThumbColor: Colors.grey[300],
-                          )
-                        ],
-                      ),
-                      TextFormField(
-                        showCursor: false,
-                        obscureText: true,
-                        style: TextStyle(fontSize: 25, color: Colors.white),
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.email, color: Colors.white, size: 25,),
-                            hintText: "Password",
-                            hintStyle: TextStyle(fontSize: 25, color: Colors.white),
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 1.5),
-                                borderRadius: BorderRadius.circular(50)
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 2),
-                                borderRadius: BorderRadius.circular(50)
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 2),
-                                borderRadius: BorderRadius.circular(50)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 25,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border.all(color: Colors.white, width: 2)
-                        ),
-                        height: 60,
-                        width: MediaQuery.of(context).size.width,
-                          child: OutlineButton(onPressed: (){}, child: Text("Login", style: TextStyle(fontSize: 25, color: Colors.white)),)),
-                      SizedBox(height: 50,),
-                      Align(
-                        alignment: Alignment.center,
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(text: 'Not a member? ', style: TextStyle(fontSize: 20, color: Colors.white),
-                              children: [
-                                TextSpan(text: 'Register!', style: TextStyle(color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold))
-                              ]
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                  padding: EdgeInsets.only(left: 30, right: 30),
-                  child: Form(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Login", style: TextStyle(fontSize: 80, color: Colors.white, fontWeight: FontWeight.bold),),
-                        SizedBox(height: 50,),
-                        TextFormField(
-                          showCursor: false,
-                          style: TextStyle(fontSize: 25, color: Colors.white),
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.email, color: Colors.white, size: 25,),
-                              hintText: "Email Address",
-                              hintStyle: TextStyle(fontSize: 25, color: Colors.white),
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 1.5),
-                                  borderRadius: BorderRadius.circular(50)
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 2),
-                                  borderRadius: BorderRadius.circular(50)
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 2),
-                                  borderRadius: BorderRadius.circular(50)
-                              )
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text('Remember me', style: TextStyle(fontSize: 20, color: Colors.white),),
-                            Switch(
-                              value: switched,
-                              onChanged: (value){
-                                setState(() {
-                                  switched = value;
-                                });
-                              },
-                              activeColor: Colors.white,
-                              inactiveTrackColor: Colors.grey[300],
-                              inactiveThumbColor: Colors.grey[300],
-                            )
-                          ],
-                        ),
-                        TextFormField(
-                          showCursor: false,
-                          obscureText: true,
-                          style: TextStyle(fontSize: 25, color: Colors.white),
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.email, color: Colors.white, size: 25,),
-                              hintText: "Password",
-                              hintStyle: TextStyle(fontSize: 25, color: Colors.white),
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 1.5),
-                                  borderRadius: BorderRadius.circular(50)
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 2),
-                                  borderRadius: BorderRadius.circular(50)
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white, width: 2),
-                                  borderRadius: BorderRadius.circular(50)
-                              )
-                          ),
-                        ),
-                        SizedBox(height: 25,),
-                        Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                border: Border.all(color: Colors.white, width: 2)
-                            ),
-                            height: 60,
-                            width: MediaQuery.of(context).size.width,
-                            child: OutlineButton(onPressed: (){}, child: Text("Login", style: TextStyle(fontSize: 25, color: Colors.white)),)),
-                        SizedBox(height: 50,),
-                        Align(
-                          alignment: Alignment.center,
-                          child: RichText(
-                            textAlign: TextAlign.center,
-                            text: TextSpan(text: 'Not a member? ', style: TextStyle(fontSize: 20, color: Colors.white),
-                                children: [
-                                  TextSpan(text: 'Register!', style: TextStyle(color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold))
-                                ]
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-              ),
-            ),
-          ],
-        )
       ),
-    ));
+    );
   }
 }
